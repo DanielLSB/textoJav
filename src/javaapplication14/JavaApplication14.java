e615b0b778598b9f9001ab896455f47a8c8af028
DanielLSB <jguerram9@miumg.edu.gt>
2021-09-19 18:34:56 -0600

    commit_txt

---
+++ src/javaapplication14/JavaApplication14.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication14;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mutsu
+ */
+public class JavaApplication14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+       //Instabciando el lector de datos
+       Visual frm=new Visual();
+       frm.show();
+    }
+    }
+  
+
